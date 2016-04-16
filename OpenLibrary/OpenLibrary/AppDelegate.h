6d4667ab37c998d5b0027896945b12b57f2cf6f6
Filipe Patricio <filipepatricio@Filipes-MBP.lan>
2016-04-16 22:30:06 +0100

    OpenLibrary App v0.1

---
+++ OpenLibrary/OpenLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenLibrary
+//
+//  Created by Filipe Patricio on 16/04/16.
+//  Copyright © 2016 Filipe Patricio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
